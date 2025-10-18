8e83c7f4f963ebc8846357d2c5a8536e24d4e917
suyundukovroman <suyundukovroman1@gmail.com>
2025-10-18 19:14:38 +0300

    update LessonManager, update console interface

--- core/lesson_manager.py
+++ core/lesson_manager.py
@@ -8,10 +8,22 @@ class LessonManager:
 
     def next_sentence(self) -> str:
         self._content_index += 1
+        segment = self._lesson.segments[self._segment_index]
+        if self._content_index >= len(segment.content):
+            if self._segment_index + 1 >= len(self._lesson.segments):
+                segment.complete = True
+                self._content_index -= 1
+                return "Lesson complete"
+            self.next_segment()
         return self.get_content_text()
 
     def previous_sentence(self) -> str:
         self._content_index -= 1
+        segment = self._lesson.segments[self._segment_index]
+        if self._content_index < 0:
+            self.previous_segment()
+            if self._segment_index < 0:
+                self._segment_index = 0
         return self.get_content_text()
 
     def get_segment_title(self) -> str:
@@ -26,3 +38,16 @@ class LessonManager:
         segment = self._lesson.segments[self._segment_index]
         return segment.content[self._content_index].get('text')
 
+    def next_segment(self):
+        segment = self._lesson.segments[self._segment_index]
+        segment.complete = True
+        self._segment_index += 1
+        self._content_index = 0
+
+    def previous_segment(self):
+        self._segment_index -= 1
+        self._content_index = 0
+
+    def is_segment_complete(self) -> bool:
+        segment = self._lesson.segments[self._segment_index]
+        return segment.complete
--- interfaces/console/main.py
+++ interfaces/console/main.py
@@ -1,6 +1,6 @@
 from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Vertical, VerticalScroll
+from textual.containers import Vertical, VerticalScroll, Horizontal
 from textual.screen import Screen
 from textual.widgets import Header, Static, Button
 
@@ -37,17 +37,33 @@ class LessonScreen(Screen):
             container.border_title = self._manager.get_segment_title()
             container.border_subtitle = self._manager.get_content_type()
             yield Static(self._manager.get_content_text(), id="lesson_text")
-            yield Button("Next" , id="next_button")
+            with Horizontal():
+                yield Button("Next" , id="next_button")
+                yield Button("Back" , id="back_button")
             yield  Button("Finish Lesson", id="finish_lesson_btn")
 
     @on(Button.Pressed, "#next_button")
     def next_button(self, event: Button.Pressed):
         self.query_one("#lesson_text", Static).update(self._manager.next_sentence())
-        self.query_one("#lesson_container", VerticalScroll).border_subtitle = self._manager.get_content_type()
+        self.lesson_container_update()
+
+    @on(Button.Pressed, "#back_button")
+    def back_button(self, event: Button.Pressed):
+        self.query_one("#lesson_text", Static).update(self._manager.previous_sentence())
+        self.lesson_container_update()
 
     @on(Button.Pressed, "#finish_lesson_btn")
     def finish_lesson_btn(self, event: Button.Pressed):
         self.app.pop_screen()
+
+    def lesson_container_update(self):
+        container = self.query_one("#lesson_container", VerticalScroll)
+        container.border_subtitle = self._manager.get_content_type()
+        container.border_title = self._manager.get_segment_title()
+        if self._manager.is_segment_complete():
+            container.styles.border = ("round", "green")
+        else:
+            container.styles.border = ("round", "white")
 
 
 if __name__ == '__main__':
